10b62156042e4ba2fb61c14b7f51808893feab85
erk1nqz <221326@astanait.edu.kz>
2024-01-30 16:07:50 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,92 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import fetch from 'node-fetch';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "pages/index.html"));
+});
+
+app.post("/weather", async (req, res) => {
+    const openweathermapAccessKey = process.env.OPENWEATHERMAP_KEY;
+
+    const { city } = req.body;
+    if (!city) {
+        return res.status(400).json({ error: 'City name is required' });
+    }
+    console.log(city);
+
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${openweathermapAccessKey}&units=metric`;
+
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        if (data.cod && data.cod !== 200) {
+            return res.status(data.cod).json({ error: data.message });
+        }
+
+        const weatherInfo = {
+            temperature: data.main.temp,
+            description: data.weather[0].description,
+            icon: data.weather[0].icon,
+            coordinates: {
+                latitude: data.coord.lat,
+                longitude: data.coord.lon
+            },
+            feelsLike: data.main.feels_like,
+            humidity: data.main.humidity,
+            pressure: data.main.pressure,
+            windSpeed: data.wind.speed,
+            countryCode: data.sys.country,
+            rainVolume: data.rain ? data.rain['1h'] : null
+        };
+
+        res.json(weatherInfo);
+
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+        res.status(500).json({ error: 'Failed to fetch weather data' });
+    }
+});
+
+app.post("/countryInfo", async (req, res) => {
+    try {
+        const { countryCode } = req.body;
+        if (!countryCode) {
+            return res.status(400).json({ error: 'Country code is required' });
+        }
+
+        const response = await fetch(`https://restcountries.com/v3.1/alpha/${countryCode}`);
+        if (!response.ok) {
+            throw new Error('Failed to fetch country data');
+        }
+        const countryData = await response.json();
+        console.log(countryData);
+        res.json(countryData);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+})
+
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "pages/about.html"));
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
